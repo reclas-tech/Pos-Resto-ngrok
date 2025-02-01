76dc93a8f0c99361a9c66f8481f89b4faf3632c5
randaru09 <ndaruanggoro123@gmail.com>
2025-02-02 02:06:33 +0700

    feat: middleware in route, and add Bearer in config

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\PrintController;
 use App\Http\Controllers\PrintTableCheckerController;
 
-Route::post('print', [PrintController::class, 'action']);
-Route::post('print/table-checker', [PrintTableCheckerController::class, 'action']);
+Route::post('print', [PrintController::class, 'action'])->middleware('reclas');
+Route::post('print/table-checker', [PrintTableCheckerController::class, 'action'])->middleware('reclas');
