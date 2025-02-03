51b737f4e087a3937f641cbc04676ac8fdd70e14
RozKoy <okkoy.1401@gmail.com>
2025-02-03 21:31:28 +0700

    fix: logic connection

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -10,63 +10,72 @@ class PrintController extends Controller
 {
     public function action(PrintReq $request)
     {
+        $data = $request->data;
+
+        $printers = [];
+        $errors = [];
+
         foreach ($request->data as $key => $item) {
             try {
                 $connector = new NetworkPrintConnector($item['ip'], 9100);
                 $printer = new Printer($connector);
-            } catch (\Exception $e) {
-                $ips = [];
-                $kitchens = [];
-                for ($i = $key; $i < count($request->data); $i++) {
-                    $ips[] = $request->data[$i]['ip'];
-                    $kitchens[] = $request->data[$i]['name'];
+
+                $printers[$item['ip']] = $printer;
+
+                $printer->setJustification(Printer::JUSTIFY_CENTER);
+
+                $printer->text("CHECKER\n");
+                $printer->text($item['name'] . "\n\n\n");
+
+                $printer->setJustification(Printer::JUSTIFY_LEFT);
+
+                $printer->text("No Transaksi  : " . $item['code'] . "\n");
+                $printer->text("Waktu         : " . $item['date'] . "\n");
+                $printer->text("Nama Pemesan  : " . $item['customer'] . "\n");
+                if (count($item['tables'] ?? []) === 0) {
+                    $printer->text("Nomor Meja    :  Take Away\n");
+                } else {
+                    $printer->text("Nomor Meja    : " . implode(',', $item['tables']) . "\n");
                 }
-                return response()->json([
-                    'message' => "Printer dengan ip [" . implode(',', $ips) . "] di [" . implode(',', $kitchens) . "] tidak ditemukan"
-                ], 500);
+
+                $printer->setJustification(Printer::JUSTIFY_CENTER);
+                $printer->text("============================================\n");
+                $printer->setJustification(Printer::JUSTIFY_LEFT);
+
+                foreach ($item['products'] as $product) {
+                    $printer->text($product['quantity'] . "x " . $product['name'] . "\n");
+                    $printer->text("=> Note : " . $product['note'] ?? '');
+                    $printer->text("\n");
+                }
+
+                $printer->setJustification(Printer::JUSTIFY_CENTER);
+                $printer->text("============================================\n");
+
+                $printer->text($item['date'] . "\n");
+                $printer->text("= Waroeng Aceh Garuda =\n");
+
+                $printer->feed(2);
+
+                if ($item['cut']) {
+                    $printer->cut();
+                }
+                $printer->close();
+            } catch (\Exception $e) {
+                $errors[$item['ip']] = $item['name'];
+
+                unset($data[$key]);
             }
-
-            $printer->setJustification(Printer::JUSTIFY_CENTER);
-
-            $printer->text("CHECKER\n");
-            $printer->text($item['name'] . "\n\n\n");
-
-            $printer->setJustification(Printer::JUSTIFY_LEFT);
-
-            $printer->text("No Transaksi  : " . $item['code'] . "\n");
-            $printer->text("Waktu         : " . $item['date'] . "\n");
-            $printer->text("Nama Pemesan  : " . $item['customer'] . "\n");
-            if(count($item['tables'] ?? [])===0){
-                $printer->text("Nomor Meja    : "." Take Away ". "\n");
-            }else{
-                $printer->text("Nomor Meja    : " . implode(',', $item['tables']) . "\n");
-            }
-
-            $printer->setJustification(Printer::JUSTIFY_CENTER);
-            $printer->text("============================================\n");
-            $printer->setJustification(Printer::JUSTIFY_LEFT);
-
-            foreach ($item['products'] as $product) {
-                $printer->text($product['quantity'] . "x " . $product['name'] . "\n");
-                $printer->text("=> Note : " . $product['note'] ?? '');
-                $printer->text("\n");
-            }
-
-            $printer->setJustification(Printer::JUSTIFY_CENTER);
-            $printer->text("============================================\n");
-
-            $printer->text($item['date'] . "\n");
-            $printer->text("= Waroeng Aceh Garuda =\n");
-
-            $printer->feed(2);
-
-            if ($item['cut']) {
-                $printer->cut();
-            }
-            $printer->close();
         }
 
-        return response()->json();
+        // foreach ($data as $key => $item) {
+        //     if ($printer = $printers[$item['ip']]) {
+                
+        //     }
+        // }
+
+        return response()->json([
+            'errors' => $errors,
+        ]);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
-use App\Http\Controllers\PrintController;
 use App\Http\Controllers\PrintTableCheckerController;
+use App\Http\Controllers\PrintController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('print', [PrintController::class, 'action'])->middleware('reclas');
 Route::post('print/table-checker', [PrintTableCheckerController::class, 'action'])->middleware('reclas');
